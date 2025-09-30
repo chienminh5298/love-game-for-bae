de9d35a8032628d872b345f3727b2cbb1750960b
Minh Nguyen <chienminh5298@gmail.com>
2025-09-29 22:50:01 -0400

    Done lv5

--- src/components/level5.js
+++ src/components/level5.js
@@ -2,21 +2,22 @@ import React, { Fragment, useState } from "react";
 import useTypewriter from "../hook/typewritter";
 import "./level.css";
 import mun from "../media/img/mun.png";
-import level4img from "../media/img/level4.png";
+import level5hint from "../media/gif/hintlv5.gif";
 
-const Level4 = ({ setLevelId }) => {
-    const { text, finished } = useTypewriter(`Đáp án này không ở trong máy. Nó nằm giữa da và giấy, nơi mỗi ngày đều nghe tiếng kim loại khẽ leng keng. Muốn gọi nó dậy, hãy tìm kị sĩ cầm gậy phi nước đại trong im lặng. Khi thấy hai chữ cái quen quen của người Pháp. Để gọi nó ra hãy nhìn thẳng vào 2 hạt nhãn long lanh và nói "Hô hô hô, khẹc khẹc khẹc"`, { speed: 50, startDelay: 500 });
+const Level5 = ({ setLevelId }) => {
+    const { text, finished } = useTypewriter(`Baby của chồng chỉ cần trả lời đúng 1 trong 3 câu hỏi.`, { speed: 50, startDelay: 500 });
 
     const [showHint, setShowHint] = useState(false);
     const [inputValue, setInputValue] = useState("");
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
+    const [selectedQuestion, setSelectedQuestion] = useState("1");
 
     const handleSubmit = () => {
-        if (inputValue.trim().toLowerCase() === "forever and always") {
-            alert("Chúc mừng bby đã vượt qua level 4. Cùng chồng qua level tiếp theo nhé!");
-            setLevelId(5);
+        if (inputValue.trim().toLowerCase() === "green" && selectedQuestion === 2) {
+            alert("Chúc mừng bby đã vượt qua level 5. Cùng chồng qua level tiếp theo nhé!");
+            setLevelId(4);
         } else {
             alert("Đáp án chưa đúng, bby thử lại nhé!");
         }
@@ -24,14 +25,19 @@ const Level4 = ({ setLevelId }) => {
     return (
         <div className="w-1/2">
             <div className="question">
-                <h1 className="text-3xl font-bold">Level 4</h1>
+                <h1 className="text-3xl font-bold">Level 5</h1>
                 <div className="mt-5 p-2 rounded-md questionBox">{text}</div>
             </div>
 
             {finished && (
                 <Fragment>
                     <div className="additionalInfo mt-5">
-                        <img src={level4img} alt="level1" className="w-1/2 rounded-xl" />
+                        <label className="block mb-2 font-medium">Chọn câu hỏi:</label>
+                        <select value={selectedQuestion} onChange={(e) => setSelectedQuestion(parseInt(e.target.value))} className="w-full px-4 py-3 rounded-lg border-2 border-gray-300 focus:border-blue-100 focus:ring-2 focus:ring-blue-300 outline-none transition duration-200 shadow-sm selectLevel5">
+                            <option value={1}>What's your mother's sister's son's nephew's neighbour name?</option>
+                            <option value={2}>What's Mun favourite color?</option>
+                            <option value={3}>What's Mun's brother current address?</option>
+                        </select>
                     </div>
                     <div className="hint mt-5">
                         <div className="hintButton p-2 rounded-sm" onClick={() => setShowHint(!showHint)}>
@@ -39,7 +45,8 @@ const Level4 = ({ setLevelId }) => {
                         </div>
                         {showHint && (
                             <Fragment>
-                                <h1 className="text-bold">Vòng này không có hint đây liu liuuuuu</h1>
+                                <h1 className="text-bold">Đoán xem hihi</h1>
+                                <img src={level5hint} alt="level1" className="w-1/2 rounded-xl" />
                             </Fragment>
                         )}
                     </div>
@@ -55,4 +62,4 @@ const Level4 = ({ setLevelId }) => {
     );
 };
 
-export default Level4;
+export default Level5;
--- src/components/levelSkeleton.js
+++ src/components/levelSkeleton.js
@@ -5,19 +5,22 @@ import Level1 from "./level1";
 import Level2 from "./level2";
 import Level3 from "./level3";
 import Level4 from "./level4";
+import Level5 from "./level5";
 
 const LevelSkeleton = () => {
-    const [levelId, setLevelId] = useState(4);
+    const [levelId, setLevelId] = useState(5);
     const navigator = useNavigate();
 
     const renderGreetingContent = () => {
         switch (levelId) {
             case 2:
-            return <Level2 setLevelId={setLevelId} />;
+                return <Level2 setLevelId={setLevelId} />;
             case 3:
                 return <Level3 setLevelId={setLevelId} />;
             case 4:
                 return <Level4 setLevelId={setLevelId} />;
+            case 5:
+                return <Level5 setLevelId={setLevelId} />;
             default:
                 return <Level1 setLevelId={setLevelId} />;
         }
